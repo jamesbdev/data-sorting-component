55705508947200a9b3c5189dab79165e61e05b38
James Bremner <j.bremner@ymail.com>
2024-11-29 12:32:13 +0000

    remove unused code

--- data-sorting-component/src/pages/index.js
+++ data-sorting-component/src/pages/index.js
@@ -3,12 +3,10 @@ import localFont from "next/font/local";
 import { Inter } from 'next/font/google';
 import Search from "@/components/Search";
 import Title from "@/components/Title";
-import CourseCard from "@/components/CourseCard";
-import data from "../../public/data.json"
 import Courses from "@/components/Courses";
 import Navigation from "@/components/Navigation";
 
-const courses = data.courses
+
 
 const inter = Inter({
   subsets: ['latin'],
