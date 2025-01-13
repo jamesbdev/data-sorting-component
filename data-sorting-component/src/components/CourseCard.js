5f9eab722e055be61221c1d35c596037c3618916
James Bremner <j.bremner@ymail.com>
2025-01-13 16:44:12 +0000

    add styling for status label

--- data-sorting-component/src/components/CourseCard.js
+++ data-sorting-component/src/components/CourseCard.js
@@ -6,6 +6,11 @@ import React from "react";
 // add colour 
 
 const CourseCard = (props) => {
+    const statusClasses = {
+        "completed": "text-green-500",
+        "in-progress": "text-yellow-500",
+        "not-started": "text-gray-500"
+    }
     return(
         <div className="course-card space-x-2.5">
             <div className="square-container">
@@ -19,7 +24,7 @@ const CourseCard = (props) => {
                 </div>
              
                 <p>{ props.description }</p>
-                <p>{ props.status }</p>
+                <p className={`status ${statusClasses[props.status.replace(" ", "-")]}`}>{ props.status }</p>
                
             </div>
          
